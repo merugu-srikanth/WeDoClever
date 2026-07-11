0e77f6051a0057b750d8a410fc18c66dcf186998
merugu-srikanth <merugusrikanth28@gmail.com>
2026-07-11 17:31:45 +0530

    Created comming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import ComingSoon from './pages/ComingSoon'
 import LandingPage from './components/LandingPage'
+import Dashbaord from './Dashbaord'
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<MainLayout />}> */}
-        <Route path="/" element={<ComingSoon />}>
-          {/* <Route index element={<LandingPage />} />   
+        <Route path="/" element={<MainLayout />}>
+        {/* <Route path="/" element={<ComingSoon />}> */}
+          <Route index element={<LandingPage />} />   
           <Route path="services" element={<Services />} />
           <Route path="projects" element={<Projects />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="coming-soon" element={<ComingSoon />} /> */}
+          <Route path="dashboard" element={<Dashbaord />} />
+          <Route path="coming-soon" element={<ComingSoon />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/Dashbaord.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Dashbaord = () => {
+    return (
+        <div className="min-h-screen mt-30 flex justify-center   text-white relative overflow-hidden">
+
+
+            <div>
+                <div className='text-2xl font-bold mb-10 text-center'>header</div>
+                <div className='flex justify-between gap-20'
+                ><div>Navabr
+                    <li>home</li>
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                    <li>BLOGS</li>
+                </div>
+                    
+                    
+                    
+                <div>Home</div></div>
+
+            </div>
+
+
+
+
+        </div>
+
+
+    )
+}
+
+export default Dashbaord
